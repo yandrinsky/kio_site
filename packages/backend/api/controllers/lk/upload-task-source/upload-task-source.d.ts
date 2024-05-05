be0e69d785188ac3627d1bbb9988a20e194208f2
Michael <mixail.zhegalin@mail.ru>
2024-05-05 15:11:48 +0300

    fix

--- packages/backend/api/controllers/lk/upload-task-source/upload-task-source.d.ts
+++ packages/backend/api/controllers/lk/upload-task-source/upload-task-source.d.ts
@@ -2,5 +2,6 @@ import { ISuccessResponse } from '../../../../domain/types';
 
 export interface IUploadTaskSourceDTO {
     taskId: string;
+    project: File;
 }
 export interface IUploadTaskSourceResponse extends ISuccessResponse {}
