75e30384deb015695175279b8fc68825281fb63a
Michael <mixail.zhegalin@mail.ru>
2024-05-07 16:40:09 +0300

    feat: добавил новую форму для отправки файлов для модуля

--- packages/frontend/src/components/user-profile-fields/user-upload-task-source-field/user-upload-task-source-field.component.tsx
+++ packages/frontend/src/components/user-profile-fields/user-upload-task-source-field/user-upload-task-source-field.component.tsx
@@ -1,6 +1,7 @@
 import { FC, useState } from 'react';
 import css from './user-upload-task-source-field.module.css';
 import { IUserUploadTaskSourceField } from './user-upload-task-source-field';
+import { Button } from '@components/ui-kit/button/button.component';
 
 export const UserUploadTaskSourceField: FC<IUserUploadTaskSourceField> = ({
   uploadTaskSource,
@@ -8,49 +9,131 @@ export const UserUploadTaskSourceField: FC<IUserUploadTaskSourceField> = ({
   isError,
   isLoading
 }) => {
+  const [moduleArchive, setModuleArchive] = useState<File>();
+  const [js1, setJs1] = useState<File>();
+  const [js2, setJs2] = useState<File>();
+
   const [isUploadTaskSource, setIsUploadTaskSource] = useState(false);
+  const isTasksSourceUploaded = Boolean(moduleArchive && js1 && js2);
 
   return (
-    <div className={css['field__container']}>
-      <div className={css['field__content']}>
+    <div className={css['container']}>
+      <div className={css['content']}>
         <div>
-          <div className={css['field__header']}>
-            <h3 className={css['field__header--h3']}>Загрузка модуля</h3>
-            <h5 className={css['field__header--h5']}>
+          <div className={css['header']}>
+            <h3 className={css['header--h3']}>Загрузка модуля</h3>
+            <h5 className={css['header--h5']}>
               Вы можете загрузить модуль для вашей задачи. Для этого нажмите на иконку загрузки
             </h5>
           </div>
 
-          {isUploadTaskSource && (
-            <div className={css['field__main']}>
-              <span className={css['field__info-text']}>
+          <div className={css['main']}>
+            {isUploadTaskSource && (
+              <span className={css['info-text']}>
                 {isLoading
-                  ? 'Идет загрузка модуля'
+                  ? 'Идет загрузка файлов'
                   : isError
-                  ? 'Во время загрузки модуля произошла ошибка!'
-                  : 'Модуль загружен успешно'}
+                  ? 'Во время загрузки файлов произошла ошибка!'
+                  : 'Файлы загружены успешно'}
               </span>
+            )}
+
+            <div className={css['loader-container']}>
+              <div className={css['loader-container__info']}>
+                <img
+                  className={css['loader-container__img']}
+                  src={moduleArchive ? '/check-mark.svg' : '/cross.svg'}
+                  alt="Иконка модуля"
+                />
+                <span>Загрузка архива модуля</span>
+              </div>
+
+              <div className={css['loader-container__img-container']}>
+                <img className={css['loader-container__img']} src="/download.svg" alt="Иконка модуля" />
+                <input
+                  className={css['input-file']}
+                  onChange={e => {
+                    if (e.target.files) {
+                      setModuleArchive(e.target.files[0]);
+
+                      e.target.value = '';
+                    }
+                  }}
+                  type="file"
+                  name="file"
+                  accept=".zip, .rar"
+                />
+              </div>
             </div>
-          )}
-        </div>
+            <div className={css['loader-container']}>
+              <div className={css['loader-container__info']}>
+                <img
+                  className={css['loader-container__img']}
+                  src={js1 ? '/check-mark.svg' : '/cross.svg'}
+                  alt="Иконка модуля"
+                />
+                <span>Загрузка js файла 1</span>
+              </div>
 
-        <div className={css['field__img-container']}>
-          <img className={css['field__img']} src="/download.svg" alt="Иконка модуля" />
-          <input
-            className={css['field__input-file']}
-            onChange={e => {
-              if (e.target.files) {
-                uploadTaskSource({ taskId, project: e.target.files[0] });
-                setIsUploadTaskSource(true);
+              <div className={css['loader-container__img-container']}>
+                <img className={css['loader-container__img']} src="/download.svg" alt="Иконка модуля" />
+                <input
+                  className={css['input-file']}
+                  onChange={e => {
+                    if (e.target.files) {
+                      setJs1(e.target.files[0]);
 
-                e.target.value = '';
-              }
-            }}
-            type="file"
-            name="file"
-            accept=".zip, .rar"
-          />
+                      e.target.value = '';
+                    }
+                  }}
+                  type="file"
+                  name="file"
+                  accept=".js"
+                />
+              </div>
+            </div>
+            <div className={css['loader-container']}>
+              <div className={css['loader-container__info']}>
+                <img
+                  className={css['loader-container__img']}
+                  src={js2 ? '/check-mark.svg' : '/cross.svg'}
+                  alt="Иконка модуля"
+                />
+                <span>Загрузка js файла 2</span>
+              </div>
+
+              <div className={css['loader-container__img-container']}>
+                <img className={css['loader-container__img']} src="/download.svg" alt="Иконка модуля" />
+                <input
+                  className={css['input-file']}
+                  onChange={e => {
+                    if (e.target.files) {
+                      setJs2(e.target.files[0]);
+
+                      e.target.value = '';
+                    }
+                  }}
+                  type="file"
+                  name="file"
+                  accept=".js"
+                />
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className={css['user-initial-footer']}>
+        <Button
+          disabled={!isTasksSourceUploaded}
+          theme="accent"
+          onClick={() => {
+            setIsUploadTaskSource(true);
+            uploadTaskSource({ taskId, project: moduleArchive! });
+          }}
+        >
+          Сохранить
+        </Button>
       </div>
     </div>
   );
